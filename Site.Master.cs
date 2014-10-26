dcb998ca5aea2fd42cbd300ada0f9df1308fa942
Harindra Herath <harindrah@gmail.com>
2014-10-26 22:33:42 +0530

    Login/Logout button added
    Login/logout functionality implemented.

--- Site.Master.cs
+++ Site.Master.cs
@@ -5,13 +5,42 @@ using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 
+using EAD_Web.Account;
+
 namespace EAD_Web
 {
     public partial class SiteMaster : System.Web.UI.MasterPage
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            LoginManager lm = new LoginManager();
 
+            if (lm.isLoggedIn(Session))
+            {
+                loginButton.Text = "Logout";
+                
+            }
+
+            else
+            {
+                loginButton.Text = "Login";
+            }
+        }
+
+        protected void loginButton_Click(object sender, EventArgs e)
+        {
+            LoginManager lm = new LoginManager();
+
+            if (lm.isLoggedIn(Session) && loginButton.Text.Equals("Logout"))
+            {
+                lm.LogOut(Session);
+                Response.RedirectPermanent("~/Account/Login.aspx", false);
+            }
+
+            else if(loginButton.Text.Equals("Login"))
+            {
+                Response.RedirectPermanent("~/Account/Login.aspx", false);
+            }
         }
     }
 }
