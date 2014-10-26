1390aa1d7868d303bc50d2aff229d86290ae31da
Harindra Herath <harindrah@gmail.com>
2014-10-26 14:33:32 +0530

    removed not null constraints on some fields

--- SQL_Scripts/ead_sql_query_make_db.sql
+++ SQL_Scripts/ead_sql_query_make_db.sql
@@ -9,14 +9,14 @@ CREATE TABLE users(
 	userid int identity primary key not null,
 	login_name varchar(50) not null,
 	firstname varchar(50) not null,
-	lastname varchar(50) not null,
-	email varchar(100) not null,
+	lastname varchar(50) ,
+	email varchar(100) ,
 	pwd varchar(64) not null,
 	nic varchar(20) not null,
-	salary float not null,
-	position varchar(100) not null,
-	primary_address varchar(200) not null,
-	bank_account_number varchar(50) not null
+	salary float ,
+	position varchar(100) ,
+	primary_address varchar(200) ,
+	bank_account_number varchar(50) 
 );
 GO
 
